d1e5d91663a6250b0f40343d6ce5f9101a3d5337
ernest nnodu <ernest.nnodu@outlook.com>
2024-11-28 15:19:08 +0000

    Create Task model and repository

--- src/main/java/com/app/todo/model/Task.java
+++ src/main/java/com/app/todo/model/Task.java
@@ -1,2 +1,18 @@
-package com.app.todo.model;public class Task {
+package com.app.todo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private boolean completed;
 }
